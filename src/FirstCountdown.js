32b640fb959bb3ab231290df49331a71fd8eb5b4
Saalik Lokhandwala <saaliklok@Saaliks-MacBook-Air.local>
2017-09-26 19:52:21 -0400

    working on countdown to the second

--- src/FirstCountdown.js
+++ src/FirstCountdown.js
@@ -3,20 +3,21 @@ import Moment from 'react-moment';
 import moment from 'moment';
 import './style.css';
 
-const diwaliDate = '2017-11-10T19:00-0500';
-const diwaliMoment = moment([2017, 11, 10, 19, 1, 1]);
-let currentMoment = moment();
-
-/*Diwali Time of the year as Seconds
-const diwaliSeconds = ;
-console.log(diwaliSeconds);*/
+const diwaliDate = '2017-11-10 19:10:10';
+const diwaliMoment = moment(diwaliDate, 'YYYY-MM-DD HH:mm:ss');
+let currentMoment = moment().unix();
 
 const diwaliDay = diwaliMoment.dayOfYear();
 const diwaliHour = diwaliMoment.hours();
 const diwaliMin = diwaliMoment.minutes();
 const diwaliSec = diwaliMoment.seconds();
 
-let diffDay = 0;
+let diffTime = diwaliMoment - currentMoment;
+console.log(diffTime);
+let duration = moment.duration(diffTime * 1000, "milliseconds");
+console.log(duration.humanize());
+
+let diffDay = duration.day;
 let diffHour= 0;
 let diffMin = 0;
 let diffSec = 0;
@@ -65,10 +66,12 @@ class FirstCountdown extends Component{
 		<div>
       		<h3>The show starts in...</h3>
       		
-      		<h3><Moment>{diwaliDate}</Moment></h3>
+      		{/*<h3><Moment>{diwaliDate}</Moment></h3>
       		<h3><Moment>{this.state.currentTime}</Moment></h3>
       		<h3>Right now it is {this.state.currentDay} days, {this.state.currentHour} hours, {this.state.currentMin} minutes, {this.state.currentSec} seconds.</h3>
-      		<h3>{diffDay} days, {diffHour} hours, {diffMin} minutes, {diffSec} seconds.</h3>
+      		*/}
+
+      		<h3 className="large">{diffDay} days!</h3>
 
 		</div>
 		)
